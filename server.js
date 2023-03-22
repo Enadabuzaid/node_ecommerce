0d90197b4d8592dfb02dc36f8b5f1409fc80f8bd
Enad Abuzaid <enad.abuzaid15@gmail.com>
2023-03-23 00:27:44 +0300

    add morgan for logg

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require("express");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+dotenv.config({path : "config.env"});
 
 const app = new express();
+console.log(process.env.NODE_ENV);
+if(process.env.NODE_ENV === 'devlopment'){
+    app.use(morgan('dev'));
+    console.log(`mode ${process.env.NODE_ENV}`);
+}
+
 
 app.get('/',(req,res) => {
  res.send('our API');
 });
 
-app.listen(8000,()=>{
-    console.log('App Running');
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>{
+    console.log(`App Running on port ${PORT}`);
 })
