5092f20e1fbfbf95962ff4a68c8aba3b73f8fe8c
Enad Abuzaid <enad.abuzaid15@gmail.com>
2023-03-24 11:58:10 +0300

    update category

--- services/categoryService.js
+++ services/categoryService.js
@@ -1,6 +1,6 @@
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
-const CategoryModel = require("../models/categoryModel");
+const Category = require("../models/CategoryModel");
 
 // @desc GET List of Categories
 // @route GET / api/vi/categories
@@ -9,7 +9,7 @@ exports.getCategories = asyncHandler(async (req, res) => {
   const page =  req.query.page * 1 || 1;
   const limit = req.query.limit * 1 || 5;
   const skip = (page - 1) * limit;
-  const categories = await CategoryModel.find({}).skip(skip).limit(limit);
+  const categories = await Category.find({}).skip(skip).limit(limit);
   res.status(201).json({ resulte: categories.length, data: categories });
 });
 
@@ -18,7 +18,7 @@ exports.getCategories = asyncHandler(async (req, res) => {
 // @access Public
 exports.getCategory = (asyncHandler (async(req, res ) => {
     const {id} = req.params
-    const category = await CategoryModel.findById(id);
+    const category = await Category.findById(id);
     if(! category){
         res.status(404).json({msg : `No Category for this id ${id}`})
     }
@@ -32,6 +32,28 @@ exports.getCategory = (asyncHandler (async(req, res ) => {
 // @access Private
 exports.createCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
-  const category = await CategoryModel.create({ name, slug: slugify(name) });
+  const category = await Category.create({ name, slug: slugify(name) });
   res.status(201).json({ data: category });
+});
+
+// @desc update Spicific Category
+// @route PUT / api/vi/categories/:id
+// @access Private
+
+exports.updateCategory = asyncHandler(async(req,res) =>{
+    const {id} = req.params;
+    const {name} = req.body;
+
+    const category = await Category.findOneAndUpdate(
+        {_id:id},
+        {name , slug: slugify(name)}, 
+        {new : true}
+    );
+
+    if(! category){
+        res.status(404).json({msg : `No Category for this id ${id}`})
+    }
+
+    res.status(200).json({data : category})
+
 });
