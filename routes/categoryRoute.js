cb829a72b3d9079bba58df598f83ae5cbdcf569d
Enad Abuzaid <enad.abuzaid15@gmail.com>
2023-03-24 11:45:58 +0300

    create and get category with get category by id

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
-const { getCategories } = require('../services/categoryService');
+const {
+    getCategories ,
+    getCategory,
+    createCategory,
+
+} = require('../services/categoryService');
 
 const router = express.Router();
 
-router.get('/', getCategories);
+router.route('/').get(getCategories).post(createCategory);
+router.route('/:id').get(getCategory);
 
 module.exports = router;
--- services/categoryService.js
+++ services/categoryService.js
@@ -1,16 +1,37 @@
-const CategoryModel = require('../models/categoryModel');
+const slugify = require("slugify");
+const asyncHandler = require("express-async-handler");
+const CategoryModel = require("../models/categoryModel");
 
-exports.getCategories = (req, res) => {
+// @desc GET List of Categories
+// @route GET / api/vi/categories
+// @access Public
+exports.getCategories = asyncHandler(async (req, res) => {
+  const page =  req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 5;
+  const skip = (page - 1) * limit;
+  const categories = await CategoryModel.find({}).skip(skip).limit(limit);
+  res.status(201).json({ resulte: categories.length, data: categories });
+});
+
+// @desc GET specific Category by id
+// @route GET / api/vi/categories/:id
+// @access Public
+exports.getCategory = (asyncHandler (async(req, res ) => {
+    const {id} = req.params
+    const category = await CategoryModel.findById(id);
+    if(! category){
+        res.status(404).json({msg : `No Category for this id ${id}`})
+    }
+
+    res.status(200).json({data : category})
+
+}));
+
+// @desc Create Category
+// @route POST / api/vi/categories
+// @access Private
+exports.createCategory = asyncHandler(async (req, res) => {
   const name = req.body.name;
-  console.log(req.body);
-
-  const newCategory = new CategoryModel({ name });
-  newCategory
-    .save()
-    .then((doc) => {
-      res.json(doc);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-};
+  const category = await CategoryModel.create({ name, slug: slugify(name) });
+  res.status(201).json({ data: category });
+});
